55cf140b3248b586af38da6d45a48170aaa2f4d7
Paul-js-hub <hesbonpoisson@gmail.com>
2023-01-17 13:11:16 +0300

    Generate token when the user is created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: { user: @user, message: "User created successfully" }, status: :created
+      token = encode_token({user_id: @user.id})
+      render json: { user: @user, token:token, message: "User created successfully" }, status: :created
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
-#   def encode_token(payload, exp = 7.days.from_now)
-#     payload[:exp] = exp.to_i
-#     JWT.encode(payload, 'mysecret')
-#   end
+  def encode_token(payload, exp = 7.days.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, 'mysecret')
+  end
+
+  def decode_token(token)
+    JWT.decode(token, 'mysecret')
+  end
 end
