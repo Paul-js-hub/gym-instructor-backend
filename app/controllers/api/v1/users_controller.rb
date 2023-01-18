3d0a936cacf09a1010e4ba5437116ec728e93327
Paul-js-hub <hesbonpoisson@gmail.com>
2023-01-18 12:56:17 +0300

    Add authenticate request method to application controller
    Add cors in order to allow cross-origin AJAX requests
    
    Fix linters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: %i[create login]
   def index
-    @users = User.all 
+    @users = User.all
     render json: @users, status: :ok
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      token = encode_token({user_id: @user.id})
-      render json: { user: @user, token:token, message: "User created successfully" }, status: :created
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token:, message: 'User created successfully' }, status: :created
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
@@ -16,15 +17,16 @@ class Api::V1::UsersController < ApplicationController
 
   def login
     @user = User.find_by(username: user_params[:username])
-    if @user && @user.authenticate(user_params[:password])
-      token = encode_token({user_id: @user.id})
-      render json: { user: @user, token:token, message: "User logged in successfully" }, status: :ok
+    if @user&.authenticate(user_params[:password])
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token:, message: 'User logged in successfully' }, status: :ok
     else
-      render json: { errors: "Invalid login credentials" }, status: :unauthorized
+      render json: { errors: 'Invalid login credentials' }, status: :unauthorized
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:full_name, :email, :username, :password)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   validates :full_name, presence: true
   validates :email, presence: true
   validates :username, presence: true, uniqueness: true
+
+  has_many :services, foreign_key: :user_id, dependent: :destroy
+  has_many :reservations, foreign_key: :user_id, dependent: :destroy
 end
