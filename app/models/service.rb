6423ac1c1f0332067a0ac94e710b1c90d8cd61ee
Alphy Gacheru <alphygacheru@gmail.com>
2023-01-25 14:50:02 +0300

    Process frontend form inputs to create a service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,11 +1,13 @@
 class Service < ApplicationRecord
+  has_one_attached :image
   has_many :reservations, dependent: :destroy
-  has_many :users, through: :reservations, dependent: :destroy
+  has_many :users, through: :reservations
 
   #  validations
   validates :title, presence: true, length: { maximum: 40 }
   validates :duration, presence: true, length: { maximum: 40 }
   validates :class_time, presence: true, length: { maximum: 40 }
   validates :fee, presence: true, length: { maximum: 40 }
-  validates :image_url, presence: true
+  validates :image, presence: true
+  validates :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_22_125537) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_113600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_125537) do
     t.string "fee"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_url"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
